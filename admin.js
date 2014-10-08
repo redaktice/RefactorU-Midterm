c199d286202e5e93f1486590ab111d326b4d1001
petercode1 <pr.gail@comcast.net>
2014-10-07 18:43:54 -0600

    Started creating login functionality

--- admin.js
+++ admin.js
@@ -1,3 +1,140 @@
-$(document).on('ready', function() {
-  
-});
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+									CLASSES
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+
+
+/*++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+					USER
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+
+/*--------------CONSTRUCTOR-----------------*/
+
+/**
+ * Creates base class for user
+ * @param {string}  firstName     First Name
+ * @param {string}  lastName     Last Name
+ * @param {string}  email     User email
+ * @param {number}  age      Age
+ * @param {image}  image    User Picture
+ * @param {Boolean} isPublic Determine if the user wants to have a public account (default private)
+ * @param {string}  username    User username for Vibe
+ * @param {string}  password    User password for Vibe
+ */
+var User = function (firstName, lastName, email, age, image, isPublic, username, password) {
+	this.firstName = firstName;
+	this.lastName = lastName;
+	this.email = email;
+	this.age = age;
+	this.media = [];
+	this.image = image;
+	this.isPublic = isPublic || false;
+	this.username = username;
+	this.password = password;
+};
+
+
+
+/*----------------METHODS-------------------*/
+
+/**
+ * Adds a facebook method to the instance of the base user class
+ * @param {object} facebook Information regarding the user's facebook account
+ */
+User.prototype.addFacebook = function (facebook) {
+	this.media.push(facebook);
+};
+
+/**
+ * Adds a twitter method to the instance of the base user class
+ * @param {object} facebook Information regarding the user's twitter account
+ */
+User.prototype.addTwitter = function (twitter) {
+	this.media.push(twitter);
+};
+
+/**
+ * Adds a instagram method to the instance of the base user class
+ * @param {object} facebook Information regarding the user's instagram account
+ */
+User.prototype.addInstagram = function (instagram) {
+	this.media.push(instagram);
+};
+
+
+
+
+/*++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+					MEDIA
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+
+/*--------------CONSTRUCTOR-----------------*/
+
+/**
+ * Creates base class for each kind of social media
+ * @param {string}  name     The kind of media
+ * @param {string}  username     Username for this media
+ * @param {string}  password      Password for this media
+ * @param {Boolean} isPublic Determine if the user wants THIS MEDIA INSTANCE to be public (default private)
+ */
+var Media = function (name, username, password, isPublic) {
+	this.name = name;
+	this.username = username;
+	this.password = password;
+	this.isPublic = isPublic || false;
+};
+
+
+/*----------------METHODS-------------------*/
+
+/**
+ * Adds a facebook method to the instance of the base user class
+ * @param {object} facebook Information regarding the user's facebook account
+ */
+var facebook = new Media('Facebook', 'h', 'd', false);
+
+
+/**
+ * Adds a twitter method to the instance of the base user class
+ * @param {object} facebook Information regarding the user's twitter account
+ */
+var twitter = new Media ('Twitter');
+
+/**
+ * Adds a instagram method to the instance of the base user class
+ * @param {object} facebook Information regarding the user's instagram account
+ */
+var instagram = new Media ('Instagram');
+
+
+
+
+/*++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+					NEW POSTS
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++*/
+
+/*--------------CONSTRUCTOR-----------------*/
+
+/**
+ * Creates base class for each kind of social media
+ * @param {string}  postTime     The time this post was created
+ * @param {Boolean}  username    Is this being sent to Facebook?
+ * @param {Boolean}  twitter    Is this being sent to Twitter?
+ * * @param {Boolean}  instagram    Is this being sent to Instagram?
+ * * @param {Array}  tags    Array of random tags that can be used to identify the post and its contents
+ * @param {Boolean} isPublic Determine if the user wants THIS MEDIA INSTANCE to be public (default private)
+ */
+var NewPost = function (postTime, facebook, twitter, instagram, tags, isPublic) {
+	this.postTime = postTime;
+	this.facebook = facebook;
+	this.twitter = twitter;
+	this.instagram = instagram;
+	this.tags = tags;
+	this.isPublic = isPublic || false;
+};
+
+
+/*----------------METHODS-------------------*/
--- app.js
+++ app.js
@@ -1,20 +1,172 @@
+
+$(document).on('ready', function() {
+  
 /*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-									CLASSES
+									LOGIN
 ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 
-/*---------------------------------CONSTRUCTOR----------------------------------*/
+$('.form-container-tall').hide();
 
-var User = function (name, age, image, isPublic) {
-	this.name = name;
-	this.age = age;
-	this.image = image;
-	this.isPublic = isPublic || false;
+/*-------------------------GLOBAL VARIABLES------------------------*/
+
+
+	var allUsers = localStorage.getItem('allData') || [];
+
+
+
+/*----------------------------FUNCTIONS----------------------------*/
+
+	/**
+	 * Checks local storage for a user and password and confirms a matach
+	 * @param  {string} inputUsername username entered in the login section
+	 * @param  {string} inputPassword password entered in the login section
+	 * @return {HTML/DOM}               If correct, loads HTML, else prompts an error
+	 */
+	var confirmUser = function(inputUsername, inputPassword) {
+
+		var failedLogin = $('.failed-login');
+
+		var confirmedUser = _.find(allUsers, function () {
+			return ((allUsers.userName === inputUsername) && (allUsers.password === inputPassword));
+		});
+
+		if (confirmedUser) {
+			// LOAD THE HOME PAGE
+		}
+
+		else {
+			failedLogin.show();
+			('.login-form input').css('background-color', '#CAA');
+		}
+	};
+
+
+/*----------------------------EVENTS----------------------------*/
+
+/**
+ * Checks user idendity and will login the user if correct
+ * @param  {event} e default event
+ * @return {Boolean}   Log-in OR Retry
+ */
+	$('#login').on('click', function (e) {
+		e.preventDefault();
+
+		var inputUsername = $('#login .input-username').val();
+		var inputPassword = $('#login .input-password').val();
+	
+		confirmUser(inputUsername, inputPassword);
+	});
+
+	/**
+	 * [Directs the user to create a new account]
+	 * @param  {[type]} e default event
+	 * @return {[DOM]}   [Shows new centered form to create a new account]
+	 */
+	$('#sign-up').on('click', function (e) {
+		e.preventDefault();
+
+		$('.form-container-avg').hide(200);
+		$('.form-container-tall').show(200);
+		
+	});
+
+
+
+	var makeNewUser = function (tempUser) {
+			
+
+			var newUser = new User(tempUser.firstName, tempUser.firstName, tempUser.email, tempUser.age, tempUser.img, tempUser.bool, tempUser.username, tempUser.password);
+
+			createUser(newUser);
+	};
+
+
+
+
+	var checkCreateComplete = function (tempUser) {
+
+		var userCreated = false;
+
+			for (var key in tempUser) {
+				if (tempUser.key === "") {
+					userCreated = false;
+				}
+			}
+	
+			if (tempUser.email.split('').indexOf('@') < 0 || tempUser.password < 8) {
+				userCreated = false;
+				console.log("UserCreated", false);
+			}
+
+			else {
+				userCreated = true;
+
+				console.log("UserCreated", false);
+			}
+	};
+
+
+	$('#create').on('click', function (e) {
+		e.preventDefault();
+
+		var inputFirstName = $('#login .input-firstname').val();
+		var inputLastName = $('#login .input-lastname').val();
+		var inputAge = $('#login .input-age').val();
+		var inputEmail = $('#login .input-email').val();
+		var inputUsername = $('#login .input-username').val();
+		var inputPassword = $('#login .input-password').val();
+
+		var tempUser = {
+			firstName: inputFirstName,
+			lastName: inputLastName,
+			email: inputEmail,
+			age: inputAge,
+			img: img,
+			bool: false,
+			username: inputUsername,
+			password: inputPassword
+		};
+
+		checkCreateComplete(tempUser);
+
+		if (userCreated) {
+
+			makeNewUser(tempUser);
+
+			// DIRECT USER TO HOME PAGE
+		}
+
+		else {
+			$('.failed-signup').toggleClass('.hidden');
+		}
+
+	});
+
+}); // --> END JQUERY
+
+
+
+
+
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+									STORAGE
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+
+/**
+ * Save information for each User
+ * @return {[object]} localStorage with values from the User
+ */
+var saveUsers = function() {
+
+	localStorage.setItem('allData', JSON.stringify(allUsers));
 };
 
-/*-----------------------------------METHODS-----------------------------------*/
-
-User.prototype.addMedia = function (media) {
-	this.media = media;
+var createUser = function (userObject) {
+	allUsers.push(userObject);
 };
+
